6fcfe0af0b2986e549935e7e0adf29c4270032cf
lovekumarshottech <love.kumar@shottech.co>
2022-04-20 00:53:29 +0500

    added storage path

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -57,7 +57,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: green;">
             <!-- Brand Logo -->
             <a href="#" class="brand-link">
-                <img src="{{ asset('admins/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ public_path('admins/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">BIZHUB</span>
             </a>
             <!-- Sidebar -->
